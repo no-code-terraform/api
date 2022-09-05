9635c9d2ea64d086a3ff002782ed5b2616469e7a
Adebayo Hountondji <mail@adebayo.fr>
2022-09-05 02:24:47 +0200

    feat: redirect index url to api index url

--- tfmaker/urls.py
+++ tfmaker/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
+
 
 from api import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', lambda r: redirect('/api/')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
