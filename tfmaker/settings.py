480966057ca17be6992493ba9a2728144eb7ae39
Adebayo Hountondji <mail@adebayo.fr>
2022-10-05 15:02:11 +0200

    feat: Remove useless itemsin  CORS_ALLOW_METHODS

--- tfmaker/settings.py
+++ tfmaker/settings.py
@@ -152,22 +152,11 @@ AUTH_USER_MODEL = 'account.User'
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_METHODS = [
-    "DELETE",
     "GET",
     "OPTIONS",
-    "PATCH",
     "POST",
-    "PUT",
 ]
 
 CORS_ALLOW_HEADERS = [
-    "accept",
-    "accept-encoding",
-    "authorization",
-    "content-type",
-    "dnt",
-    "origin",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
+    "*",
 ]
